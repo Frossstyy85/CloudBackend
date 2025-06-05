c0c3b4d395e3faae5b60a1d07937c07abd0eeaa2
Frossstyy85 <morgan.lindholm@gmail.com>
2025-06-05 22:10:10 +0200

    idk

--- src/main/java/com/example/cloudbackend/config/AwsConfig.java
+++ src/main/java/com/example/cloudbackend/config/AwsConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
